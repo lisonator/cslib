9b34641fc0aa8f5ed3270cc183e0f0fd25eb9469
Jakub Lisowski <lisonator@gmail.com>
2012-08-29 19:25:26 +0200

    version 0.21 added circular buffer

---
+++ circularBuffer.c
@@ -0,0 +1,56 @@
+#include "circularBuffer.h"
+#include <string.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+int initializeCb(struct CircularBuffer *this,
+        void *buffer, int nElements, int eSize, int mode)
+{
+    this->buffer = buffer;
+    this->end = (char*)buffer + nElements*eSize;
+    this->nElements = nElements;
+    this->eSize = eSize;
+    this->mode = mode;
+    this->head = buffer;
+    this->tail = buffer;
+    this->stored = 0;
+    return(EXIT_SUCCESS);
+}
+
+int popCb(struct CircularBuffer *this,
+        void *item)
+{
+   if(this->stored == 0)
+       return(EXIT_FAILURE);
+   int i;
+   memcpy(item, this->tail, this->eSize);
+   this->tail = (char*)this->tail + this->eSize;
+   if(this->tail == this->end)
+       this->tail = this->buffer;
+   this->stored--;
+   return(EXIT_SUCCESS);
+}
+
+int pushCb(struct CircularBuffer *this,
+        const void *item)
+{   
+    memcpy(this->head, item, this->eSize);
+    this->head = (char*)this->head + this->eSize;
+    if(this->head == this->end)
+        this->head = this->buffer;
+    this->stored++;
+    if(this->stored>this->nElements){
+        this->stored = this->nElements;
+        this->tail = this->head;
+        return(EXIT_FAILURE);
+        //EXIT_FAILURE means that data has been overwritten
+    }
+    return(EXIT_SUCCESS);
+}
+void flush(struct CircularBuffer *this)
+{
+    this->head = this->buffer;
+    this->tail = this->buffer;
+    this->stored = 0;
+}
---
+++ circularBuffer.h
@@ -0,0 +1,41 @@
+/* 
+ * File:   circularBuffer.h
+ * Author: Jakub Lisowski
+ *
+ * Created on 29 sierpień 2012, 17:13
+ */
+
+#ifndef CIRCULARBUFFER_H
+#define	CIRCULARBUFFER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    struct CircularBuffer{
+        void *buffer;
+        void *end;
+        int nElements;
+        int eSize;
+        int mode;
+        int stored;
+        void *head;
+        void *tail;
+    };
+
+    int initializeCb(struct CircularBuffer*,
+            void *buffer, int nElements, int eSize, int mode);
+    
+    int popCb(struct CircularBuffer*,
+            void *item);
+    int pushCb(struct CircularBuffer*,
+            const void *item);
+    void flush(struct CircularBuffer*);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CIRCULARBUFFER_H */
+
